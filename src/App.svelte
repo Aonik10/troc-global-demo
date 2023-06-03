b93e20e5e3d033bbb172a89f2256dd34853e028a
Emiliano <emp10999@gmail.com>
2023-06-03 20:40:50 -0300

    change graph library and other changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Chart from 'chart.js/auto';
-	import { afterUpdate, onMount } from 'svelte';
+	import { Bar, Line } from 'svelte-chartjs'
+	import { onMount } from 'svelte';
 	import { getHistoricalMarketData } from './api/resources';
 	
 	let coins = ['bitcoin', 'ethereum', 'binancecoin', 'cardano', 'poligon']
@@ -10,34 +11,10 @@
 	let days = 90;
 
 	// se definen estas variables y sus valores se asignan en onMount
-	let prices; 
-	let volume;
-	let labels;
+	let prices = []; 
+	let volume = [];
+	let labels = [];
 	let max_days; 
-
-	let pricesData;
-	let volumeData;
-	let currentLabels;
-	let pricesChart;
-	let pricesCanvas;
-	let volumeCanvas;
-	let volumeChart;
-
-	function setConfig(type, data) {
-		const config = {
-			type: type,
-			data: data,
-			options: {
-				animations: false,
-				elements: {
-                    point:{
-                        radius: 0
-					}
-				}
-			}
-		};
-		return config
-	}
 	
 	function buildGraphData(labels, data) {
 		const graphData = {
@@ -45,7 +22,7 @@
 			datasets: [
 				{
 					label: 'Prices',
-					data: data.slice(-days),
+					data: data,
 					fill: true,
 					backgroundColor: 'rgb(75, 192, 192)',
 					borderColor: 'rgb(29, 82, 82)',
@@ -53,51 +30,30 @@
 				},
 			]
 		};
-		return graphData
+		return graphData;
 	}
 
-	afterUpdate(() => {
-		console.log(currentCoin)
-		if (pricesChart) {
-			pricesChart.destroy()
-		}
-		if (volumeChart) {
-			volumeChart.destroy()
-		}
-		currentLabels = labels.slice(-days)
-		pricesData = buildGraphData(currentLabels, prices)
-		pricesChart = new Chart(pricesCanvas, setConfig('line', pricesData));
-		volumeData = buildGraphData(currentLabels, volume)
-		volumeChart = new Chart(volumeCanvas, setConfig('bar', volumeData));
-		console.log("after")
-	})
-
-	onMount(async ()=> {
+	let updateData = async () => {
 		// hago el fetch de toda la md una sola vez al montar el componente
-		let response = await getHistoricalMarketData(currentCoin, currentCurrency, 'max')
-		console.log(response)
+		let response = await getHistoricalMarketData(currentCoin, currentCurrency, 'max');
 
 		// guardo la md en variables 
-		prices = response.prices.map(p => p[1])
-		volume = response.total_volumes.map(p => p[1])
-		labels = response.prices.map(p => new Date(p[0]).toLocaleDateString("en-US"))
-		max_days = response.prices.length
+		prices = response.prices.map(p => p[1]);
+		volume = response.total_volumes.map(p => p[1]);
+		labels = response.prices.map(p => new Date(p[0]).toLocaleDateString("en-US"));
+		max_days = response.prices.length;
+	}
 
-		// proceso la información que deseo mostrar
-		currentLabels = labels.slice(-days)
-		pricesData = buildGraphData(currentLabels, prices)
-		volumeData = buildGraphData(currentLabels, volume)
+	$: labelsSlice = labels.slice(-days);
+	$: pricesData = buildGraphData(labelsSlice, prices.slice(-days));
+	$: volumeData = buildGraphData(labelsSlice, volume.slice(-days));
 
-		pricesCanvas.getContext("2d")
-		volumeCanvas.getContext("2d")
-		pricesChart = new Chart(pricesCanvas, setConfig('line', pricesData));
-		volumeChart = new Chart(volumeCanvas, setConfig('bar', volumeData));
-	})
-
+	onMount(updateData);
+	
 </script>
 
 <main class="main">
-	<h1>COIN TRAKER</h1>
+	<h1>COIN TRACKER</h1>
 	<div>
 		<div class="inputs-container">
 			<div class="input-item">
@@ -106,32 +62,40 @@
 			</div>
 			<div class="input-item">
 				<label for="coin-input">Coin</label>
-				<select name="coin-select" id="coin-select" bind:value={currentCoin}>
+				<select name="coin-select" id="coin-select" bind:value={currentCoin} on:change={updateData}>
 					{#each coins as coin}
 						<option value={coin}>{coin}</option>
 					{/each}
 				</select>
 			</div>
 			<div class="input-item">
-				<label for="currency-input">Coin</label>
-				<select name="coin-select" id="coin-select" bind:value={currentCurrency}>
+				<label for="currency-input">Currency</label>
+				<select name="currency-select" id="currency-select" bind:value={currentCurrency} on:change={updateData}>
 					{#each currencies as currency}
 						<option value={currency}>{currency}</option>
 					{/each}
 				</select>
 			</div>
 		</div>
-		<button class="btn-update">Update</button>
+		<button class="btn-update" on:click={updateData}>Update</button>
 	</div>
 	<div class="graphs-section">
-		
 		<div class="graph-container">
-			<p>days {days}</p>
-			<canvas bind:this={pricesCanvas} id="pricesGraph" width={4} height={2} />
+			days {days}
+			<Line
+				data={pricesData}
+				width={100}
+				height={50}
+				options={{ animations: false, elements: { point: { radius: 0 }}}}
+			/>
 		</div>
 		<div class="graph-container">
-			<p>days {days}</p>
-			<canvas bind:this={volumeCanvas} id="pricesGraph" width={4} height={2} />
+			<Bar
+				data={volumeData}
+				width={100}
+				height={50}
+				options={{ animations: false, elements: { point: { radius: 0 }}}}
+			/>
 		</div>
 	</div>
 </main>
--- test.js
+++
@@ -1,4 +0,0 @@
-let timestamp = 1685750400000
-let date = new Date(timestamp).toLocaleDateString("en-US")
-
-console.log(date)
